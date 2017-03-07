f52e84ccdaec1943d0566334061b13842d665d34
Gabriel Gizotti <gabriel@gizotti.com>
2017-03-08 00:49:01 +1000

    add circle.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@
 
 require 'simplecov'
 
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start do
   # Remove spec files from the coverage report for a more accurate
   # coverage percentage
